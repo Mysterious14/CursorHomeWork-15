74a59ecd3acb6104169214d7a014d8831f0349b4
Юрій Ірза <irzaurij04@gmail.com>
2022-10-12 18:23:23 +0300

    first add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+
+const ANAKIN_IMAGE =
+  'https://www.bobafettfanclub.com/multimedia/galleries/albums/userpics/10001/anakin-skywalker-1569312236.jpeg';
+
+const RAY_IMAGE =
+  'https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale';
+
+const verified =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Twitter_Verified_Badge.svg/1200px-Twitter_Verified_Badge.svg.png';
+const posts = [
+  {
+    name: 'Anakin Skywalker',
+    photo: ANAKIN_IMAGE,
+    nickname: '@dart_vader',
+    official: true,
+    content: 'WTF? Who is Ray? Why she is Skywalker? Luke...?',
+    image: RAY_IMAGE,
+    date: '26 лют.',
+    coments: 15,
+    likes: 25,
+    downloads: 2,
+    shares: 7,
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img id="authotPhoto" src={posts[0].photo} alt="photoAuthor"></img>
+      <p id="nameAuthor">{posts[0].name}</p>
+      <img className="verified" src={posts[0].official ? verified : ''} alt="verified"></img>
+      <p className="nickname">{posts[0].nickname}</p>
+      <p className="date">{posts[0].date}</p>
+      <p className="content">{posts[0].content}</p>
+      <img id="image" src={posts[0].image} alt="post"></img>
+      <div className="icons">
+        <p className="icon">&#10084; {posts[0].likes}</p>
+        <p className="icon">&#128488; {posts[0].coments}</p>
+        <p className="icon">🚀 {posts[0].shares}</p>
+        <p className="icon">📥 {posts[0].downloads}</p>
+      </div>
     </div>
   );
 }
